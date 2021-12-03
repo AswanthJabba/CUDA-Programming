9cc11aa2f7ebb951d587a57a540c11b4f37c8631
AswanthJabba <86704864+AswanthJabba@users.noreply.github.com>
2021-12-03 09:31:46 +0530

    Create addition.cu

---
+++ src/addition.cu
@@ -0,0 +1,25 @@
+/*
+This CUDA program performs addition using kernel function 
+*/
+#include<stdio.h>
+__global__ void add(int a, int b, int *d_c)
+{
+    *d_c = a + b;
+}
+int main()
+{
+    int a,b,c;
+    int *d_c;
+    a=3;
+    b=4;
+    // Allocating memory for device pointer of integer
+    cudaMalloc((void**)&d_c, sizeof(int));
+    // Calling Kernel function
+    add<<<1,1>>>(a,b,d_c);
+    // Copying the result from device to host
+    cudaMemcpy(&c, d_c, sizeof(int), cudaMemcpyDeviceToHost);
+    printf("%d + %d is %d\n", a, b, c);
+    // Free the device pointer
+    cudaFree(d_c);
+    return 0;
+}
